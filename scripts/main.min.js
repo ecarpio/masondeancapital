d87d9f405f711eae884c41d881856c36055543f4
edcarpio <Ed.Carpio@Nafinc.com>
2019-03-04 11:37:57 -0800

    first commit

---
+++ scripts/main.min.js
@@ -0,0 +1,12 @@
+$(function(){
+    console.log('test sdfdf')
+
+
+    var options = {
+        mobileFallbackImage: "http://www.hdwallpapers.in/walls/pink_cosmos_flowers-wide.jpg",
+        playOnlyIfVisible  : false
+    };
+
+    myPlayer = jQuery(".player").YTPlayer(options);
+
+})
