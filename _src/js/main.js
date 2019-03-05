1097e771d82d9cb723e44fe4140f35d746e88254
Ed <carpio.ed@gmail.com>
2019-03-04 23:44:06 -0800

    Building Homepage Stucture

--- _src/js/main.js
+++ _src/js/main.js
@@ -1,12 +1,10 @@
 $(function(){
-    console.log('test sdfdf')
-
 
     var options = {
         mobileFallbackImage: "http://www.hdwallpapers.in/walls/pink_cosmos_flowers-wide.jpg",
         playOnlyIfVisible  : false
     };
 
-    myPlayer = jQuery(".player").YTPlayer(options);
+    myPlayer = $(".player").YTPlayer(options);
 
 })
